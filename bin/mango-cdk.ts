6f3b9222ac59079cf5d851b372f1c55aac25447f
Gowtham Mallikarjuna <ggwm@amazon.com>
2021-11-25 21:18:48 -0800

    updated stage account

--- bin/mango-cdk.ts
+++ bin/mango-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { MangoCdkStack } from '../lib/mango-cdk-stack';
+import { PipelineStack } from '../lib/mango-cdk-stack';
 
 const app = new cdk.App();
-new MangoCdkStack(app, 'MangoCdkStack', {
+new PipelineStack(app, 'MangoCdkStack', {
   env: {
     account: '147866640792',
     region: 'us-west-2',
--- lib/mango-cdk-stack.ts
+++ lib/mango-cdk-stack.ts
@@ -1,39 +1,38 @@
 import * as cdk from '@aws-cdk/core';
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
-import { Construct, SecretValue, Stack, Stage, StageProps } from '@aws-cdk/core';
+import { Construct, Stage, StageProps } from '@aws-cdk/core';
 import { MyLambdaStack } from './lambda-stack';
 import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
 
 class MyApplication extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
-
     const lambdaStack = new MyLambdaStack(this, 'lambda');
   }
 }
 
-export class MangoCdkStack extends cdk.Stack {
+export class PipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: "MangofruityCDK",
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.connection('gowtham91m/mango-cdk', 'main', {
-          connectionArn: 'arn:aws:codestar-connections:us-west-2:147866640792:connection/4b18bea2-9eb6-47b1-bbdc-adb3bf6fd2a9',
+    const pipeline = new CodePipeline(this, 'Pipeline',
+      {
+        crossAccountKeys: true,
+        pipelineName: "MangofruityCDK",
+        synth: new ShellStep('Synth', {
+          input: CodePipelineSource.connection('gowtham91m/mango-cdk', 'main', {
+            connectionArn: 'arn:aws:codestar-connections:us-west-2:147866640792:connection/4b18bea2-9eb6-47b1-bbdc-adb3bf6fd2a9',
+          }),
+          commands: [
+            'npm ci',
+            'npm run build',
+            'npx cdk synth',
+          ],
         }),
-        commands: [
-          'npm ci',
-          'npm run build',
-          'npx cdk synth',
-        ],
-      }),
-    });
+      });
 
-    pipeline.addStage(new MyApplication(this, 'Prod', {
+    pipeline.addStage(new MyApplication(this, 'Staging', {
       env: {
-        account: '147866640792',
+        account: '697393813417',
         region: 'us-west-2',
       },
     }));
