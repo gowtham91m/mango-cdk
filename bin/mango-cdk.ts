01e7839cc8551220803b3c521d48ecbf48ceb921
Gowtham Mallikarjuna <ggwm@amazon.com>
2021-11-25 21:30:44 -0800

    update stack name

--- bin/mango-cdk.ts
+++ bin/mango-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { PipelineStack } from '../lib/mango-cdk-stack';
+import { PipelineStack } from '../lib/mango-pipeline-stack';
 
 const app = new cdk.App();
-new PipelineStack(app, 'MangoCdkStack', {
+new PipelineStack(app, 'PipelineStack', {
   env: {
     account: '147866640792',
     region: 'us-west-2',
--- test/mango-cdk.test.ts
+++ test/mango-cdk.test.ts
@@ -1,13 +1,13 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as MangoCdk from '../lib/mango-cdk-stack';
+import * as MangoCdk from '../lib/mango-pipeline-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new MangoCdk.MangoCdkStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new MangoCdk.PipelineStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    "Resources": {}
+  }, MatchStyle.EXACT))
 });
