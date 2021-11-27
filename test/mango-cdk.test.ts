9b578aafe38ec42de054a44f7cd368506de371bc
Gowtham Mallikarjuna <ggwm@amazon.com>
2021-11-27 13:02:29 -0800

    bug fix, updated test stack

--- test/mango-cdk.test.ts
+++ test/mango-cdk.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as MangoCdk from '../lib/mango-pipeline-stack';
+import { PipelineStack } from '../lib/pipeline-stack/pipeline-stack';
 
 test('Empty Stack', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new MangoCdk.PipelineStack(app, 'MyTestStack');
+  const stack = new PipelineStack(app, 'MyTestStack');
   // THEN
   expectCDK(stack).to(matchTemplate({
     "Resources": {}
