6a98f611d321712b5869425af4407a074607251d
Gowtham Mallikarjuna <ggwm@amazon.com>
2023-09-23 14:41:13 -0700

    updated pipeline stack name

--- bin/mango-cdk.ts
+++ bin/mango-cdk.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new PipelineStack(app, 'Mangotrails', {
+new PipelineStack(app, 'MangotrailsPipeline', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
